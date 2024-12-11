d027455ee507042c7de7f5ab00f3f512fe17dd60
aleks <aht477@gmail.com>
2024-12-11 14:25:55 +0100

    lesson_26 add

---
+++ src/lesson_26/GenExample.java
@@ -0,0 +1,46 @@
+package lesson_26;
+
+public class GenExample <K, V>{
+    private K key;
+    private V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public GenExample(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+
+    }
+
+    @Override
+    public String toString() {
+        return "GenExample{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        GenExample<Integer, String> genExample = new GenExample<>(100, "String");
+
+        System.out.println(genExample);
+
+        GenExample<String, String> stringStringGenExample = new GenExample<>("Java", "Hello");
+
+        System.out.println(stringStringGenExample);
+    }
+}
